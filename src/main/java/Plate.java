d65ac95469e477987f2a0aeff5348cde7876336e
Artjom_Islyamov_notebook <pixeltom@yandex.ru>
2021-06-07 19:23:16 +0300

    Last version

--- src/main/java/Plate.java
+++ src/main/java/Plate.java
@@ -1,2 +1,25 @@
 public class Plate {
+    private int food;
+
+    public Plate (int food) {
+        this.food = food;
+    }
+
+
+    boolean decreaseFood (int n) {
+        int foodInPlate = food - n;
+        if (foodInPlate < 0) return false;
+        food -= n;
+        return true;
+    }
+
+    public void info () {
+        System.out.println("plate: " + food);
+    }
 }
+
+
+
+
+
+
